b79dedd8b8ecb9f90b696ab10eee5c385537552c
daniel Gil <danielgilcalahorrano@gmail.com>
2022-03-04 18:41:11 +0100

    Arreglo en tablas y insert de temporadas (Faltan las rutas de las imagenes)

---
+++ BBDD/Inserts/InsertTablaTemporadas.sql
@@ -0,0 +1,5 @@
+use API_Cultura;
+INSERT INTO Temporadas VALUES('Primavera','/assets/','Marzo/Abril/Mayo/Junio')
+INSERT INTO Temporadas VALUES('Verano','/assets/','Junio/Julio/Agosto/Septiembre')
+INSERT INTO Temporadas VALUES('Otoño','/assets/','Septiembre/Obtubre/Noviembre/Diciembre')
+INSERT INTO Temporadas VALUES('Invierno','/assets/','Diciembre/Enero/Febrero/Marzo')
--- BBDD/Tablas/CrearTablaOpiniones.sql
+++ BBDD/Tablas/CrearTablaOpiniones.sql
@@ -6,6 +6,6 @@ Apellidos VARCHAR(150),
 Email VARCHAR(150),
 Telefono INTEGER,
 Calificacion INTEGER,
-Comentario VARCHAR
+Comentario VARCHAR(max)
 CONSTRAINT Pk_Opiniones PRIMARY KEY (Id),
 );
