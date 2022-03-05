2e144285886a9f75d121d513bf507c3a4fb251f9
daniel Gil <danielgilcalahorrano@gmail.com>
2022-03-05 15:34:46 +0100

    Insertar Proveedores y cambio en "telefono"

---
+++ BBDD/Inserts/InsertTablaProveedores.sql
@@ -0,0 +1,17 @@
+use API_Cultura
+
+INSERT INTO Proveedores VALUES('José Luis O.','Calahorra', '34 611 11 11 11')
+
+INSERT INTO Proveedores VALUES('Manolo R.','Calahorra', '34 611 11 11 11')
+
+INSERT INTO Proveedores VALUES('Santiago I.','Arnedo', '34 611 11 11 11')
+
+INSERT INTO Proveedores VALUES('Marcos T','Alfaro', '34 611 11 11  11')
+
+INSERT INTO Proveedores VALUES('Alfredo D.','Préjano','34 611 11 11  11')
+
+INSERT INTO Proveedores VALUES('Ignacio G.','Arnedillo','34 611 11 11  11')
+
+INSERT INTO Proveedores VALUES('Luis P.','Autol','34 611 11 11  11')
+
+INSERT INTO Proveedores VALUES('Paco L.','Calahorra', '34 611 11 11  11')
--- BBDD/Tablas/CrearTablaProveedores.sql
+++ BBDD/Tablas/CrearTablaProveedores.sql
@@ -3,6 +3,6 @@ CREATE TABLE Proveedores(
 Id INTEGER IDENTITY(1,1),
 Nombre VARCHAR(100),
 Poblacion VARCHAR(75),
-Telefono INTEGER,
+Telefono VARCHAR(30),
 CONSTRAINT Pk_Proveedores PRIMARY KEY (Id)
 );
