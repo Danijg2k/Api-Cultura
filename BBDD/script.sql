de3826cfa33fa366b61d51d2a6fc4f1f8166091e
daniel Gil <danielgilcalahorrano@gmail.com>
2022-03-16 16:39:30 +0100

    Cambios en la cabecera y BBDD insert de imagenes de temporadas

--- BBDD/script.sql
+++ BBDD/script.sql
@@ -130,10 +130,10 @@ INSERT INTO Provee VALUES(4, 8, 0.25)
 
 --Insertar datos Tabla Temporadas
 
-INSERT INTO Temporadas VALUES('Primavera','/assets/Temporadas/primavera.jpg','Marzo/Abril/Mayo/Junio')
-INSERT INTO Temporadas VALUES('Verano','/assets/Temporadas/verano.jpg','Junio/Julio/Agosto/Septiembre')
-INSERT INTO Temporadas VALUES('Otoño','/assets/Temporadas/otono.jpg','Septiembre/Octubre/Noviembre/Diciembre')
-INSERT INTO Temporadas VALUES('Invierno','/assets/Temporadas/invierno.jpeg','Diciembre/Enero/Febrero/Marzo')
+INSERT INTO Temporadas VALUES('Primavera','/assets/Temporadas/Primavera.jpg','Marzo/Abril/Mayo/Junio')
+INSERT INTO Temporadas VALUES('Verano','/assets/Temporadas/Verano.jpg','Junio/Julio/Agosto/Septiembre')
+INSERT INTO Temporadas VALUES('Otoño','/assets/Temporadas/Otono.jpg','Septiembre/Octubre/Noviembre/Diciembre')
+INSERT INTO Temporadas VALUES('Invierno','/assets/Temporadas/Invierno.jpg','Diciembre/Enero/Febrero/Marzo')
 
 --Insertar datos Tabla Pertenece
 
